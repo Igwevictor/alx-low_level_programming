2b3af599d972646ecf665e0b715956262d8be17f
Igwe victor <igwevictor5007@gmail.com>
2022-12-14 17:54:17 -0800

    4 is alpha c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,19 +1,11 @@
-#include "holberton.h"
-/**
- * _isalpha - Check Main
- * @c: An input character
- * Description: function returns 1 if the character is a
- * letter, lowercase or uppercase.
- * Return: 1 or 0 in otherwise.
- */
-int _isalpha(int c)
-{
-	char lower, upper;
-	int isletter = 0;
+#include "main.h"
 
-	for (lower = 'a'; lower <= 'z'; lower++)
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: the character to be checked
+ * Return: 1 if c is a letter, 0 otherwise
+ */
+	int _isalpha(int c)
 	{
-		for (upper = 'A'; upper <= 'Z'; upper++)
-		{
-			if (c == lower || c == upper)
-				isletter =:wq
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
