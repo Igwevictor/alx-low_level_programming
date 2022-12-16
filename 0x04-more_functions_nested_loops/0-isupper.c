3c486426d5c6a3752cab21be2f6fd24ec9fc527a
Igwe victor <igwevictor5007@gmail.com>
2022-12-16 11:01:43 -0800

    0isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,21 @@
+/*
+ * File: 0-isupper.c
+ * Auth: Suara Ayomide
+ */
+
 #include "main.h"
 
 /**
- * _isupper - uppercase character
- * @c: char to check
+ * _isupper - Checks for uppercase characters.
+ * @c: The character to be checked.
  *
- * Return: 0 or 1
+ * Return: 1 if character is uppercase, 0 otherwise.
  */
-
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 		return (1);
+
 	else
 		return (0);
 }
